bdb9bbf6a4d1a8f07f43a554b8942e691bcc59aa
undefined <azmolwebautomation@gmail.com>
2024-08-05 17:40:36 +0300

    bug_fix[server]: client disconnect on server failure

--- client.py
+++ client.py
@@ -2,8 +2,11 @@ import asyncio
 
 
 class Client(asyncio.Protocol):
+
     def connection_made(self, transport):
         self.transport = transport
+        self.loop = asyncio.get_event_loop()
+        self._checker = self.loop.create_task(self.check_connection())
         print("Connected to server")
 
     def data_received(self, data):
@@ -11,20 +14,40 @@ class Client(asyncio.Protocol):
 
     def connection_lost(self, exc):
         print("The server closed the connection")
+        self._checker.cancel()  # Cancel the periodic connection check task
+        if exc:
+            print(f"Connection lost with error: {exc}")
+        else:
+            print("Connection closed cleanly")
         self.transport.close()
+        self.loop.stop()  # Stop the event loop
+
+    async def check_connection(self):
+        try:
+            while True:
+                # print('check connection here')
+                await asyncio.sleep(1)
+        except asyncio.CancelledError:
+            pass
 
 
 async def main():
     loop = asyncio.get_running_loop()
-    future = loop.create_future()
 
-    transport, protocol = await loop.create_connection(Client, '127.0.0.1', 8000)
+    client = Client
+    transport, protocol = await loop.create_connection(client, '127.0.0.1', 8000)
 
     try:
-        await future
-    except Exception:
+        await asyncio.Future()
+    except Exception as e:
+        print(f"Exception in main: {e}")
         transport.abort()
+    finally:
+        transport.close()
 
 
 if __name__ == '__main__':
-    asyncio.run(main())
+    try:
+        asyncio.run(main())
+    except Exception:
+        print("Client stopped")
